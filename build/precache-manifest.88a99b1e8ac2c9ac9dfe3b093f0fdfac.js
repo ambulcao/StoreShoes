46862627e9e2913b72acd864e08144c7210f03e9
Alexandre Bulcao <ambulcao@gmail.com>
2021-01-12 16:34:01 +0000

    Update build

---
+++ build/precache-manifest.88a99b1e8ac2c9ac9dfe3b093f0fdfac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7726495721f370d119d3e0229f196e4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fe237b584734f6f192a",
+    "url": "/static/css/2.ffcf707b.chunk.css"
+  },
+  {
+    "revision": "3fe237b584734f6f192a",
+    "url": "/static/js/2.f4a85834.chunk.js"
+  },
+  {
+    "revision": "b3c8206db83680ffbd86",
+    "url": "/static/js/main.3d0dfd48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "38ea808a8078ca62133217454397665b",
+    "url": "/static/media/background.38ea808a.svg"
+  },
+  {
+    "revision": "728ca744e85782f34168ead6ee9ed1c8",
+    "url": "/static/media/logo.728ca744.svg"
+  }
+]);
